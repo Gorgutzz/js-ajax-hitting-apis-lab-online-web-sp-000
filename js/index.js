977f0b1b253f54730a13a213aa800a586ba8daa3
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-22 00:24:20 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // your code here
+function getRepositories() {
+    const username = document.getElementById("username").value;
+    const req = new XMLHttpRequest();
+    req.addEventListener('load', displayRepositories);
+    req.open("GET", `https://api.github.com/users/${username}/repos`);
+    req.send();
+  }
+
+  function displayRepositories() {
+    var repos = JSON.parse(this.responseText);
+    console.log(repos);
+    const repoList = `<ul>${repos
+      .map(
+        r =>
+          '<li>' +
+          r.name +
+          ' - <a href="#" data-repo="' +
+          r.name +
+          '" onclick="getCommits(this)">Get Commits</a></li>'
+      )
+      .join('')}</ul>`;
+    document.getElementById('repositories').innerHTML = repoList;
+  }
