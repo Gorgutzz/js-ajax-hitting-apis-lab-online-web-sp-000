edf82d88ee4a376aada51fa0c428aa5c1a034f4d
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-22 00:31:19 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -12,16 +12,19 @@ function getRepositories() {
     const repoDisplay =
       '<ul>' +
       repos.map(repo => {
-        const dataUsername = 'data-username="' + repo.owner.login + '"';
-        const dataRepoName = 'data-repository="' + repo.name + '"';
-        return
-        <li>
-          <h4>${repo.name}</h4>
-          <a href="${repo.html_url}">${repo.html_url}</a><br>
-          <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-          <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
-        </li>`;
-      })
-      .join('')}</ul>`;
-    document.getElementById('repositories').innerHTML = repoList;
-  }
+          const dataUsername = 'data-username="' + repo.owner.login + '"';
+          const dataRepoName = 'data-repository="' + repo.name + '"';
+          return `
+            <li>
+              <h4>${repo.name}</h4>
+              <a href="${repo.html_url}">${repo.html_url}</a><br>
+              <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+              <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+            </li>`;
+        })
+        .join('') +
+      '</ul>';
+    document.getElementById('repositories').innerHTML = repoDisplay;
+}
+
+  
